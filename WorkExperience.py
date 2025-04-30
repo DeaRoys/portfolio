889b682661f66e7ada947016562563bf860458a2
Dea Roys <112359705+DeaRoys@users.noreply.github.com>
2025-04-30 23:22:37 +1000

    Create WorkExperience.py

---
+++ WorkExperience.py
@@ -0,0 +1,6 @@
+## Work Experience 1/ Internship
+- Working in the database part of a project, to create a mobile version of a website.
+- Moving data between projects within a system.
+- Review any existing documentation about the functionality of the existing application.
+- Meet with client to fully elaborate on the functionality of existing application.
+- Analyzed the old system database and compared it with the new data model.
